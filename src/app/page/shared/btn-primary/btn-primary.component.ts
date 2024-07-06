5aae9f3cb53293ad6553c3c1308b592021296ba1
Aldo Souza <aldocnsouza@gmail.com>
2024-07-06 16:59:31 -0300

    feat: add btn-primary

---
+++ src/app/page/shared/btn-primary/btn-primary.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'btn-primary',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+
+  @Input("btn-text") btnText: string = " ";
+
+}
