7a12e7ce43d5814dbf73b69730ee21a06c23fcb7
Aldo Souza <aldocnsouza@gmail.com>
2024-07-06 18:39:57 -0300

    feat: add new components

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { MainComponent } from '../shared/main/main.component';
+import { AboutMeComponent } from '../shared/about-me/about-me.component';
+import { MyProjectsComponent } from '../shared/my-projects/my-projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    AboutMeComponent,
+    MyProjectsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
