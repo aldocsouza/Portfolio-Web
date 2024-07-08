3a75a179929a64ba1a256114ae4158053d4b1c03
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 20:34:24 -0300

    ref: ajuste do texto

--- src/app/page/shared/about-me/about-me.component.ts
+++ src/app/page/shared/about-me/about-me.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'about-me',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
 
+  @Input() darkAndLighMode: boolean = false;
+  @Input() portAndEng: boolean = false;
 }
