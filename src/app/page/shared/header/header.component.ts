9d432e82ae6eee5ac0f9fe8028c309d41a6e076c
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 21:30:26 -0300

    fix: botões de testes foram comentados para não aparecerem e causar bugs

--- src/app/page/shared/header/header.component.ts
+++ src/app/page/shared/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'header-component',
   standalone: true,
   imports: [
-    BtnPrimaryComponent
+    BtnPrimaryComponent,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @Input() darkAndLighMode: boolean = false;
+  @Input() portAndEng: boolean = false;
+
+  @Output() darkAnLight = new EventEmitter();
+  @Output() portAndEnglish = new EventEmitter();
+
+  onDarkAndLight(event: boolean){
+    this.darkAnLight.emit(event)
+  }
+  onPortAndEng(event: boolean){
+    this.portAndEnglish.emit(event)
+  }
 
 }
