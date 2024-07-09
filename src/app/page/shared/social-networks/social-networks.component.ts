2fe18cf87c8d57692d30200feded7dbbe66fdb1d
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 21:33:15 -0300

    feat: add social networks component + assets

---
+++ src/app/page/shared/social-networks/social-networks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'social-networks',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-networks.component.html',
+  styleUrl: './social-networks.component.scss'
+})
+export class SocialNetworksComponent {
+
+}
