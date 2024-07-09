2745495a9b84fd3523cc628476ec37f6089264d8
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 21:35:56 -0300

    ref: add todos os componentes na Home

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { MainComponent } from '../shared/main/main.component';
 import { AboutMeComponent } from '../shared/about-me/about-me.component';
-import { MyProjectsComponent } from '../shared/my-projects/my-projects.component';
+import { ProjectCardComponent } from '../shared/project-card/project-card.component';
+import { Repository } from '../model/Repository';
+import { GithubService } from '../shared/services/github.service';
+import { CommonModule } from '@angular/common';
+import { SocialNetworksComponent } from '../shared/social-networks/social-networks.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+export interface RepositoryId{
+  id: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -11,11 +20,61 @@ import { MyProjectsComponent } from '../shared/my-projects/my-projects.component
     HeaderComponent,
     MainComponent,
     AboutMeComponent,
-    MyProjectsComponent
+    ProjectCardComponent,
+    SocialNetworksComponent,
+    FooterComponent,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  @Input() darkAndLighMode: boolean = false;
+  @Input() portAndEng: boolean = false;
+
+  repositories: Repository[] = []
+
+  languages: any[] = []
+
+  repoId: RepositoryId[] = [
+    {id : 733305561},
+    {id : 742028443},
+    {id : 805531031},
+    {id : 824334447}
+  ]
+
+  constructor(
+    private github: GithubService
+  ){}
+
+  darkMode(event: boolean){
+    this.darkAndLighMode = event;
+  }
+
+  portAndEnglish(event: boolean){
+    this.portAndEng = event;
+  }
+
+  ngOnInit(): void {
+    this.github.getRepo().subscribe({
+      next: response => {
+        response.forEach(repo => {
+          for(let repository of this.repoId){
+            if(repo.id === repository.id){
+              this.repositories.push(repo)
+              this.getUrlLanguage(repo.languages_url)
+            }
+          }
+        })
+      }
+    })
+  }
+
+  getUrlLanguage(url: string){
+    this.github.getLanguages(url).subscribe(r => {
+        this.languages.push(r)
+    })
+  }
 
 }
---
+++ src/app/page/model/Repository.ts
@@ -0,0 +1,11 @@
+export interface Repository{
+  id: number;
+  name: string;
+  git_url: string;
+  description: string;
+  url: string;
+  language: string;
+  languages_url: string;
+}
+
+
