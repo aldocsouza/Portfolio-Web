81bb4bad70ad1583f939b182566693dcdf446b72
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 21:31:06 -0300

    ref: correção no estylo dos texto para responsividade

--- src/app/page/shared/main/main.component.ts
+++ src/app/page/shared/main/main.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'main-component',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    BtnPrimaryComponent
+    BtnPrimaryComponent,
+    CommonModule
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  @Input() darkAndLighMode: boolean = false;
+  @Input() portAndEng: boolean = false;
 
 }
