01132bf336b6d3ace65ada0092a8de83197ce5f0
Aldo Souza <aldocnsouza@gmail.com>
2024-07-08 21:28:30 -0300

    ref: mudança em alguns inputs para o botão

--- src/app/page/shared/btn-primary/btn-primary.component.ts
+++ src/app/page/shared/btn-primary/btn-primary.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'btn-primary',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss'
 })
 export class BtnPrimaryComponent {
 
-  @Input("btn-text") btnText: string = " ";
+  @Input("btn-text") btnText: string = "";
+  @Input() style: string = ""
+
+  @Output() submit = new EventEmitter();
+  @Output() portAndEng = new EventEmitter();
+
+  trueAndFalse: boolean = false;
+  truePortAndEng: boolean = false;
+
+  onSubmit(){
+    this.trueAndFalse = !this.trueAndFalse
+    this.submit.emit(this.trueAndFalse)
+  }
+
+  onPortAndEng(){
+    this.truePortAndEng = !this.truePortAndEng
+    this.portAndEng.emit(this.truePortAndEng)
+  }
 
 }
